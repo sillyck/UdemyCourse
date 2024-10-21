665264971e962315ac35d56ea2e32debba7dd8b2
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-10-21 12:55:20 +0200

    Hola mon and Hola mon bean in json format

--- src/main/java/com/jordiribellas/udemy/demo/holamon/HolaMon.java
+++ src/main/java/com/jordiribellas/udemy/demo/holamon/HolaMon.java
@@ -1,15 +1,20 @@
 package com.jordiribellas.udemy.demo.holamon;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // REST API
 @RestController
 public class HolaMon {
 	
-	// /hola-mon
-	
+	@GetMapping("/hola-mon")
 	public String holaMon() {
 		return "Hola mon";
+	}
+	
+	@GetMapping("/hola-mon-bean")
+	public holaMonBean holaMonBean() {
+		return new holaMonBean("Hola mon bean");
 	}
 	
 }
---
+++ src/main/java/com/jordiribellas/udemy/demo/holamon/holaMonBean.java
@@ -0,0 +1,17 @@
+package com.jordiribellas.udemy.demo.holamon;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class holaMonBean {
+
+	private String message;
+}
