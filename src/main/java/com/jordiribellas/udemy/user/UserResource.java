e60cde86dd54a916a4bc6ec61a8c0e9a47341abd
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-10-24 12:29:03 +0200

    Improved the create user method adding a location url after creating a user

--- src/main/java/com/jordiribellas/udemy/user/UserResource.java
+++ src/main/java/com/jordiribellas/udemy/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.jordiribellas.udemy.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -32,7 +35,15 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.saveUser(user);
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User saved = service.saveUser(user);
+		
+		//users/3 => /users/{id}
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(saved.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
