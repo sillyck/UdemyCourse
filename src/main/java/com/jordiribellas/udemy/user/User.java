4dfd5d1c26ce064ff1249b2f1dfd1bfb75beb63c
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-10-28 12:48:06 +0100

    Errors controled and we print the specific error

--- src/main/java/com/jordiribellas/udemy/user/User.java
+++ src/main/java/com/jordiribellas/udemy/user/User.java
@@ -18,9 +18,9 @@ import lombok.ToString;
 public class User {
 	private Integer id;
 	
-	@Size(min = 2)
+	@Size(min = 2, message = "Name should have at least 2 characters")
 	private String name;
 	
-	@Past
+	@Past (message = "Birth Date should be in the past")
 	private LocalDate birthDate;
 }
