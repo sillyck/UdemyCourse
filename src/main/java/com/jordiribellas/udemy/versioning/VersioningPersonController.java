bfd64bef28b662e9047332ccbee675fc495ef790
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-10-29 12:55:01 +0100

    Added /person/header with headers, Added /person/accept with produces

--- src/main/java/com/jordiribellas/udemy/versioning/VersioningPersonController.java
+++ src/main/java/com/jordiribellas/udemy/versioning/VersioningPersonController.java
@@ -26,5 +26,25 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionOfPersonRequestPrameter() {
 		return new PersonV2(new Name("Javier", "Ibarra"));
 	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1", produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Javier Ibarra");
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2", produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Javier", "Ibarra"));
+	}
 
+	@GetMapping(path = "/person/accept", produces = { "application/vnd.company.app-v1+json", MediaType.APPLICATION_JSON_VALUE})
+	public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+		return new PersonV1("Javier Ibarra");
+	}
+	
+	@GetMapping(path = "/person/accept", produces = { "application/vnd.company.app-v2+json", MediaType.APPLICATION_JSON_VALUE})
+	public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+		return new PersonV2(new Name("Javier", "Ibarra"));
+	}
+	
 }
