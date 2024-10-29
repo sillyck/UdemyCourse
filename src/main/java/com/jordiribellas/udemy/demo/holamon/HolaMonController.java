c80b3614ade6337b804bc49562225942dd0f7e68
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-10-29 12:09:28 +0100

    Added i18n, catalan, spanish and english

--- src/main/java/com/jordiribellas/udemy/demo/holamon/HolaMonController.java
+++ src/main/java/com/jordiribellas/udemy/demo/holamon/HolaMonController.java
@@ -1,5 +1,9 @@
 package com.jordiribellas.udemy.demo.holamon;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API
 @RestController
 public class HolaMonController {
+	
+	private MessageSource messageSource;
+	
+	public HolaMonController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping("/hola-mon")
 	public String holaMon() {
@@ -28,5 +38,11 @@ public class HolaMonController {
 		return new holaMonBean(String.format("Hola mon, %s", name));
 	}
 	
+	@GetMapping("/hola-mon-wolrdwide")
+	public String holaMonWolrdWide() {
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		return messageSource.getMessage("bon.dia.message", null, "Default Message", locale );
+	}
 	
 }
